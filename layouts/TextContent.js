1e45b1d8e185abf564e2dfc43cfc5a4d69ac3935
schneidersec <chris@schneidersec.com>
2021-04-04 11:55:13 -0500

    fixed weird text padding

--- layouts/TextContent.js
+++ layouts/TextContent.js
@@ -2,7 +2,7 @@ import { Text } from "@chakra-ui/react"
 
 const TextContent = ({ children, ...rest }) => {
    return (
-        <Text padding="5px" {...rest}>
+        <Text {...rest}>
             {children}
         </Text>
    )    
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
-import { Box, ChakraProvider, Flex, Spacer, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme'
 import DeskNavbar from '../components/Navbar/DeskNavbar';
 import MobileNav from '../components/Navbar/MobileNav';
 import Footer from '../components/Navbar/Footer';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
+
   return (
     <ChakraProvider theme={theme} resetCSS>
         <Box
